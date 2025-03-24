a020ba50015560bb86b3567e1c4c424db34bb0f7
otavio.silva <otavioaraujo490@gmail.com>
2025-03-24 09:21:17 -0300

    add User schema for user management functionality

---
+++ src/auth/schema/user.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop({ default: mongoose.mongo.ObjectId })
+  id: string;
+
+  @Prop({ required: true })
+  userName: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: '#123@123#' })
+  password: string;
+
+  @Prop({
+    default: 'https://docs.nestjs.com/assets/logo-small-gradient.svg',
+  })
+  picture: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
