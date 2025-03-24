1f6c78c553f38ea3d20b5184ab9c2badd4d0d560
otavio.silva <otavioaraujo490@gmail.com>
2025-03-24 09:57:57 -0300

    add create user endpoint with validation in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,9 +4,11 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Post,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { User } from './schema/user.schema';
 
@@ -33,6 +35,32 @@ export class AuthController {
 
       return await this.authService.login(loginUserDto);
     } catch (error: unknown) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'An unexpected error occurred';
+      const errorStatus =
+        (error as { status?: number })?.status ||
+        HttpStatus.INTERNAL_SERVER_ERROR;
+      throw new HttpException(errorMessage, errorStatus);
+    }
+  }
+
+  @ApiOperation({ summary: 'Create User' })
+  @ApiResponse({ status: 200, description: 'User created successfully' })
+  @ApiResponse({
+    status: 400,
+    description: 'User already exists',
+  })
+  @Post('create')
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    try {
+      if (!createUserDto.username || !createUserDto.password) {
+        throw new HttpException(
+          'Username and password are required',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      return await this.authService.createUser(createUserDto);
+    } catch (error) {
       const errorMessage =
         error instanceof Error ? error.message : 'An unexpected error occurred';
       const errorStatus =
