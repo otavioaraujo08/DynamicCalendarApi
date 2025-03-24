a8992fc27d91badc1f0df75da2d960c5c44e3bb6
otavio.silva <otavioaraujo490@gmail.com>
2025-03-24 09:51:35 -0300

    add AuthController for user login functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { User } from './schema/user.schema';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({ status: 200, description: 'User logged in' })
+  @Get('login')
+  async login(@Body() loginUserDto: LoginUserDto): Promise<User> {
+    try {
+      return await this.authService.login(loginUserDto);
+    } catch (error: unknown) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'An unexpected error occurred';
+      const errorStatus =
+        (error as { status?: number })?.status ||
+        HttpStatus.INTERNAL_SERVER_ERROR;
+      throw new HttpException(errorMessage, errorStatus);
+    }
+  }
+}
