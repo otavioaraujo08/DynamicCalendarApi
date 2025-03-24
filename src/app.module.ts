70d8e5172bfcf38587e874942e8dbe21f3fd49f4
otavio.silva <otavioaraujo490@gmail.com>
2025-03-24 11:12:30 -0300

    add MongooseModule for MongoDB connection in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG_PASSWORD}@dynamiccalendarcluster.d8fzz.mongodb.net/?retryWrites=true&w=majority&appName=DynamicCalendarCluster`,
+    ),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
