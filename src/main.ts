24f5b308672d6634dad85d01b87049b3c1955853
otavio.silva <otavioaraujo490@gmail.com>
2025-03-25 09:30:56 -0300

    add error handling during application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -18,4 +18,6 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Error during application bootstrap:', error);
+});
