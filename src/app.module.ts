7cd6c2aaa70118c4c5cd7a6ef70d86fad5951286
otavio.silva <otavioaraujo490@gmail.com>
2025-03-25 09:28:21 -0300

    add Schedule schema to AppModule for schedule management integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { Schedule } from './schedule/schema/schedule.schema';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
       `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG_PASSWORD}@dynamiccalendarcluster.d8fzz.mongodb.net/?retryWrites=true&w=majority&appName=DynamicCalendarCluster`,
     ),
     AuthModule,
+    Schedule,
   ],
   controllers: [AppController],
   providers: [AppService],
