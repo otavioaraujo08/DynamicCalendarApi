b6999b2cc6a383642a1b758557a1bc266c4c6f4b
otavio.silva <otavioaraujo490@gmail.com>
2025-03-25 09:26:57 -0300

    add CreateScheduleDto for schedule creation with necessary properties

---
+++ src/schedule/dto/create-schedule.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateScheduleDto {
+  @ApiProperty()
+  readonly date: string;
+  @ApiProperty()
+  readonly startTime: string;
+  @ApiProperty()
+  readonly endTime: string;
+  @ApiProperty()
+  readonly dayOfTheWeek: string;
+  @ApiProperty()
+  readonly title: string;
+  @ApiProperty()
+  readonly description: string;
+  @ApiProperty()
+  readonly createdBy: string;
+}
