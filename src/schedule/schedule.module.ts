576d989b03dcd5926c539d985bd3b16cd38d0abd
otavio.silva <otavioaraujo490@gmail.com>
2025-03-25 09:27:40 -0300

    add ScheduleModule for managing schedule-related components and dependencies

---
+++ src/schedule/schedule.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { ScheduleController } from './schedule.controller';
+import { ScheduleService } from './schedule.service';
+import { Schedule, ScheduleSchema } from './schema/schedule.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Schedule.name, schema: ScheduleSchema },
+    ]),
+    AuthModule,
+  ],
+  controllers: [ScheduleController],
+  providers: [ScheduleService],
+})
+export class ScheduleModule {}
