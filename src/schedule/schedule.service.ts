7e381b56c5f5d3c37b9e5b543fee1cdea67f9b6f
otavio.silva <otavioaraujo490@gmail.com>
2025-03-25 09:27:26 -0300

    add ScheduleService for managing schedule operations including CRUD functionality

---
+++ src/schedule/schedule.service.ts
@@ -0,0 +1,127 @@
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AuthService } from 'src/auth/auth.service';
+import { CreateScheduleDto } from './dto/create-schedule.dto';
+import { ScheduleDto } from './dto/schedule.dto';
+import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { Schedule, ScheduleDocument } from './schema/schedule.schema';
+
+@Injectable()
+export class ScheduleService {
+  private readonly logger = new Logger(ScheduleService.name);
+
+  constructor(
+    @InjectModel(Schedule.name) private scheduleModel: Model<ScheduleDocument>,
+    private readonly authService: AuthService,
+  ) {}
+
+  async getAllSchedules(): Promise<ScheduleDto[]> {
+    this.logger.log('Getting all schedules');
+    const schedules = await this.scheduleModel.find().exec();
+    this.logger.log(`Found ${schedules.length} schedules`);
+    return schedules;
+  }
+
+  async getScheduleById(id: string): Promise<ScheduleDto | null> {
+    this.logger.log(`Getting schedule with id: ${id}`);
+    const schedule = await this.scheduleModel.findById(id).exec();
+    if (!schedule) {
+      this.logger.error(`Schedule with id: ${id} not found`);
+      return null;
+    }
+    this.logger.log(`Found schedule with id: ${id}`);
+    return schedule;
+  }
+
+  async createSchedule(schedule: CreateScheduleDto): Promise<Schedule> {
+    this.logger.log('Creating schedule');
+
+    const requiredFields = [
+      'date',
+      'startTime',
+      'endTime',
+      'dayOfTheWeek',
+      'title',
+      'description',
+      'createdBy',
+    ];
+    for (const field of requiredFields) {
+      if (!CreateScheduleDto[field]) {
+        this.logger.error(`Missing required field: ${field}`);
+        throw new BadRequestException(`Missing required field: ${field}`);
+      }
+    }
+    const userExists = await this.authService.findUserById(schedule.createdBy);
+    if (!userExists) {
+      this.logger.error(`User with id: ${schedule.createdBy} not found`);
+      throw new NotFoundException(
+        `User with id: ${schedule.createdBy} not found`,
+      );
+    }
+
+    const createdSchedule = new this.scheduleModel(schedule);
+    this.logger.log(`Schedule created: ${schedule.title}`);
+    return createdSchedule.save();
+  }
+
+  async updateSchedule(
+    id: string,
+    schedule: UpdateScheduleDto,
+  ): Promise<Schedule> {
+    this.logger.log(`Updating schedule with id: ${id}`);
+
+    const requiredFields = [
+      'date',
+      'startTime',
+      'endTime',
+      'dayOfTheWeek',
+      'title',
+      'description',
+      'createdBy',
+      'updatedBy',
+    ];
+    for (const field of requiredFields) {
+      if (!CreateScheduleDto[field]) {
+        this.logger.error(`Missing required field: ${field}`);
+        throw new BadRequestException(`Missing required field: ${field}`);
+      }
+    }
+
+    const userExists = await this.authService.findUserById(schedule.updatedBy);
+    if (!userExists) {
+      this.logger.error(`User with id: ${schedule.updatedBy} not found`);
+      throw new NotFoundException(
+        `User with id: ${schedule.updatedBy} not found`,
+      );
+    }
+
+    const updatedSchedule = await this.scheduleModel
+      .findByIdAndUpdate(id, schedule, { new: true })
+      .exec();
+    if (!updatedSchedule) {
+      this.logger.error(`Schedule with id: ${id} not found`);
+      throw new NotFoundException(`Schedule with id: ${id} not found`);
+    }
+    this.logger.log(`Schedule updated: ${schedule.title}`);
+    return updatedSchedule;
+  }
+
+  async deleteSchedule(id: string): Promise<Schedule> {
+    this.logger.log(`Deleting schedule with id: ${id}`);
+    const deletedSchedule = await this.scheduleModel
+      .findByIdAndDelete(id)
+      .exec();
+    if (!deletedSchedule) {
+      this.logger.error(`Schedule with id: ${id} not found`);
+      throw new NotFoundException(`Schedule with id: ${id} not found`);
+    }
+    this.logger.log(`Schedule deleted: ${deletedSchedule.title}`);
+    return deletedSchedule;
+  }
+}
